602b5ee1c82022285b94840514f5bf752789f4d5
Matias <mati.adagio@gmail.com>
2019-08-17 12:34:18 -0300

    .

--- ARDUINO/Mensaje_Post/Mensaje_Post.ino
+++ ARDUINO/Mensaje_Post/Mensaje_Post.ino
@@ -1,59 +1,33 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-#include <WiFiClient.h> 
-#include <ESP8266WebServer.h>  
-#include <WiFiManager.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
 //-------------------VARIABLES GLOBALES--------------------------
-int contconexion = 0;
 
-const char *ssid = "Javo wifi";//"Fibertel WiFi159 2.4GHz";//"AndroidAP";//"Speedy-0F574D";//"Fibertel WiFi159 2.4GHz";//"SO Avanzados";//"Fibertel WiFi159 2.4GHz";
-const char *password = "44540006";//"0043442422";//"matiasmanda";//"6761727565";//"0043442422";//"SOA.2019";//"0043442422";
-
-unsigned long previousMillis = 0;
-
-char* host = "www.gestiondenegocio.esy.es";//"192.168.1.34";//"www.gestiondenegocio.esy.es";
-String strhost = "www.gestiondenegocio.esy.es";//"192.168.1.34";//"www.gestiondenegocio.esy.es";
-String strurl = "/index.php";
-String chipid = "";
-boolean con = false;
-int cantUser = 0;
-String mensaje = "";
-String opcion = "";
-int PinARD = D1; 
-int PinARDIn = D2; 
-volatile long pulses = 0;
-int flag = 0;
-static volatile unsigned long debounce = 0; // Tiempo del rebote.
-const int interruptPin = 0;
-int id = 100;
-String ruta_prueba = "D0|F400|D90|F400|D90|F400|D90#";
+const char *ssid = "Fibertel WiFi159 2.4GHz";//"Javo wifi";//"AndroidAP";//"Speedy-0F574D";//"SO Avanzados";
+const char *password = "0043442422";//"44540006";//"matiasmanda";//"6761727565";//"SOA.2019";
 ESP8266WebServer server(80);   
-WiFiManager wifimanager;
-
-
-String peticionPOSTJSON(){
+//WiFiManager wifimanager;
+String chipID = "";
+String peticionPOSTJSON(int op,int dato){
   
-    String linea = "error";
     const size_t capacity = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+    char* host = "www.gestiondenegocio.esy.es";//"192.168.1.34";//"www.gestiondenegocio.esy.es";
+    const char* response = "";
+    String linea = "error";
     DynamicJsonDocument JSONencoder(capacity);
     DeserializationError err;
-    JSONencoder["OPCION"] = 12;
-    JSONencoder["ID"] = ESP.getChipId();
-    /*
-    JsonArray values = JSONencoder.createNestedArray("values"); //JSON array
-    values.add(20); //Add value to array
-    values.add(21); //Add value to array
-    values.add(23); //Add value to array
-    */
+    JSONencoder["OPCION"] = op;//12;
+    JSONencoder["ID"] = dato;//ESP.getChipId();
+
     WiFiClient http;    //Declare object of class HTTPClient
-  if (!http.connect(host, 80)) {
+    if (!http.connect(host, 80)) {
       Serial.println("Fallo de conexion");
       return linea;
     }
     http.print("POST"); http.print(" "); http.print("/"); http.println(" HTTP/1.1");
-    http.print("HOST: "); http.println(strhost);
+    http.print("HOST: "); http.println(host);
     //http.println("User-Agent: BePIM");
     size_t len = measureJson(JSONencoder);
     http.println("Content-Type: application/json");
@@ -81,9 +55,13 @@ String peticionPOSTJSON(){
     err = deserializeJson(JSONencoder,linea);   
     if (!err) 
     {           
-        if(JSONencoder["codigo"] == "ENVIO"){//{"codigo":"ENVIO","dato":"-D0|F400|D90|F400|D90|F400|D90#"}
-          const char* ruta = JSONencoder["dato"];
-          Serial.println(ruta); 
+          if(JSONencoder["codigo"] == "ENVIO"){//{"codigo":"ENVIO","dato":"-D0|F400|D90|F400|D90|F400|D90#"}
+            response = JSONencoder["dato"];
+            Serial.println(response); 
+          }
+          if(JSONencoder["codigo"] == "REGISTRO"){
+            response = JSONencoder["dato"];
+            Serial.println(response); 
           }          
     }
     else
@@ -94,22 +72,61 @@ String peticionPOSTJSON(){
     return linea;  
 }
 
+void entrenamientoPOST()
+{
+    //StaticJsonBuffer<500> jsonBuffer;
+    const char* response = "";
+    const size_t capacity = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+    String post_body = server.arg("plain");
+    Serial.println(post_body);
+    DynamicJsonDocument root(capacity);
+      
+    DeserializationError err = deserializeJson(root,post_body);
+    
+    if (!err) {   
+        if (server.method() == HTTP_POST){
+            if(root["opcion"] == "PRUEBA"){
+              response = root["codigo"];
+              Serial.println(response);
+              server.send(200, "text/plain", "Respuesta ESP32");         
+              } // 1           
+        }
+    }
+    else{
+        Serial.println("error in parsin json body");
+        server.send(400);
+    }
+    
+}
+
+ void config_rest_server_routing() {
+    server.on("/training", HTTP_POST, entrenamientoPOST);
+}
+
 void setup() {
 
   Serial.begin(115200);
-  WiFi.begin (ssid, password); 
-  //wifimanager.resetSettings();
-  wifimanager.autoConnect("BePIM");
-
+  
+  WiFi.begin (ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Conectando...");
   }
 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  peticionPOSTJSON(2,ESP.getChipId());
+  config_rest_server_routing();
+  server.begin();                                              //Inicializa el servidor (una vez configuradas las rutas)
+  Serial.println("Servidor HTTP inicializado"); 
 }
 
 void loop() {
-  peticionPOSTJSON();
+  /**Metodo que se ejecuta para el entrenamiento, en donde el modulo queda a la espera de la proxima instruccion**/
+  server.handleClient(); //espero a que algun cliente se conecte y realice una peticion
+  /**Metodo que se ejecuta durante la operativa**/
+  //peticionPOSTJSON(12,ESP.getChipId());
   delay(2000);
 
 }
